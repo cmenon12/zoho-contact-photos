c187a943377e678c4aba1865f17bcd43466760eb
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2020-12-30 21:18:03 +0000

    Added upload_photo() and a config file.
    upload_photo() will upload the supplied photo for the contact.
    Added config file using configparser, primarily to hold the CSRF token.
    Would like to find this from the cookies to save the user from manually
    locating it.
    
    Signed-off-by: cmenon12 <16004217+cmenon12@users.noreply.github.com>

--- update-photos.py
+++ update-photos.py
@@ -1,5 +1,11 @@
-from pycookiecheat import chrome_cookies
+import configparser
+
 import requests
+from pycookiecheat import chrome_cookies
+
+__author__ = "Christopher Menon"
+__credits__ = "Christopher Menon"
+__license__ = "gpl-3.0"
 
 # How many contacts to fetch in each request
 PAGE_SIZE = 100
@@ -17,10 +23,11 @@ def fetch_contacts() -> list:
         print("Downloading page %d..." % page_number)
         url = "https://contacts.zoho.com/api/v1/accounts/self/contacts?page=%d&per_page=%d" % (page_number, PAGE_SIZE)
         cookies = chrome_cookies(url)
-        response = requests.get(url, cookies=cookies).json()
+        response = requests.get(url, cookies=cookies)
+        response.raise_for_status()
 
         # Save each contact
-        for contact in response["contacts"]:
+        for contact in response.json()["contacts"]:
 
             # Guarantee that these keys are present
             if "first_name" not in contact.keys():
@@ -31,33 +38,63 @@ def fetch_contacts() -> list:
             contacts.append(contact)
 
         # Iterate
-        has_more = response["has_more"]
+        has_more = response.json()["has_more"]
         page_number += 1
 
     return contacts
 
 
-def locate_photo(contact: dict):
+def locate_photo(contact: dict, config: configparser.ConfigParser):
+    photos_uploaded_count = 0
 
+    # Attempt to find the photo at first_name last_name.jpg
     try:
-        filename = "./photos/" + contact["first_name"] + " " + contact["last_name"] + ".jpg"
-        photo = open(filename, "rb")
+        filepath = "./%s/%s %s.jpg" % (config["photo_folder"], contact["first_name"], contact["last_name"])
+        photo = open(filepath, "rb")
+        upload_photo(contact, photo, config["csrf_token"])
+        photos_uploaded_count += 1
         photo.close()
     except FileNotFoundError:
-        try :
-            filename = "./photos/" + contact["first_name"] + "  " + contact["last_name"] + ".jpg"
-            photo = open(filename, "rb")
+
+        # If not, try to find the photo at first_name  last_name.jpg
+        try:
+            filepath = "./%s/%s  %s.jpg" % (config["photo_folder"], contact["first_name"], contact["last_name"])
+            photo = open(filepath, "rb")
+            upload_photo(contact, photo, config["csrf_token"])
+            photos_uploaded_count += 1
             photo.close()
         except FileNotFoundError:
-            print("Error: No picture found for %s %s" % (contact["first_name"], contact["last_name"]))
+            print("Error: No photo found for %s %s" % (contact["first_name"], contact["last_name"]))
+
+    print("%d photos were uploaded" % photos_uploaded_count)
+
+
+def upload_photo(contact: dict, photo: str, csrf_token: str):
+    # Make the request
+    url = "https://mail.zoho.com/zm/zc/api/v1/accounts/%s/contacts/%s/photo" % (contact["zid"], contact["contact_id"])
+    cookies = chrome_cookies(url)
+    files = {"photo": photo}
+    headers = {'x-zcsrf-token': csrf_token}
+    response = requests.post(url, cookies=cookies, files=files, headers=headers)
+    response.raise_for_status()
+
+    if response.json()["status_code"] == 200 and response.json()["message"] == "Photo Uploaded":
+        print("Photo updated for %s %s" % (contact["first_name"], contact["last_name"]))
+    SystemExit
+
+    pass
 
 
 def main():
+    # Load the configuration
+    parser = configparser.ConfigParser()
+    parser.read("config.ini")
+    config = parser["update-photos"]
+
     contacts = fetch_contacts()
 
     for contact in contacts:
-        locate_photo(contact)
-
+        locate_photo(contact, config)
 
 
 if __name__ == "__main__":
