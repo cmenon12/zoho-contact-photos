680d76fdb25b3aa5d31cea42569fedbb0ff1f049
Christopher Menon <16004217+cmenon12@users.noreply.github.com>
2025-05-02 20:40:13 +0100

    Add User-Agent header to the session

--- update_photos.py
+++ update_photos.py
@@ -35,6 +35,7 @@ PHOTOS_FOLDER = "photos"
 
 # The session to use for all requests
 SESSION = requests.Session()
+SESSION.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0"})
 
 
 def get_cookies(website: str) -> dict[str, str]:
